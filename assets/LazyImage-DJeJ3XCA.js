42bf038b9ba56dcf70c4ce070cf08b468208f00c
someoneelse-entirely <someoneelse-entirely@users.noreply.github.com>
2024-12-14 14:47:38 +0000

    deploy: ca54dc840d4b047f355e990c9ac2fab964f4d811

---
+++ assets/LazyImage-DJeJ3XCA.js
@@ -0,0 +1,1 @@
+import{r as o,j as r}from"./index-DAq1Erfw.js";function u({src:t,alt:e,className:a}){const[,s]=o.useState(!1);return r.jsx("img",{src:t,alt:e,className:a,onLoad:()=>s(!0)})}export{u as default};
